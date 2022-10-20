5602de37cca9022dc91ec3ed99de1605a5c9c2cd
Linxae <702611663@qq.com>
2022-10-20 23:46:34 +0800

    🔥throttle基本实现

--- js/de&th/_throttle.js
+++ js/de&th/_throttle.js
@@ -1,4 +1,12 @@
 //v1
-function() {
-
+function throttle(fn, interval) {
+	let lastTime = 0;
+	const _throttle = function (...args) {
+		let nowTime = new Date().getTime();
+		if (interval - (nowTime - lastTime) <= 0) {
+			fn.apply(this, args);
+			lastTime = nowTime;
+		}
+	};
+	return _throttle;
 }
