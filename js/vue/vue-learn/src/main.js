76a9094e5dc0e8cce1632a1036280e3f67097341
Linxae <702611663@qq.com>
2022-11-10 13:02:25 +0800

    watchEffect

--- js/vue/vue-learn/src/main.js
+++ js/vue/vue-learn/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import "../assets/init.css";
 // import App from "./01-vue3-watch-1/App.vue";
-import App from "./02-vue3-debounceRef/App.vue";
+// import App from "./02-vue3-debounceRef/App.vue";
+import App from "./03-vue3-watchEffect/App.vue";
 
 createApp(App).mount("#app");
