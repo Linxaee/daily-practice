a9248a167f6e430d45b1cb6aeea1373583a36f10
Linxae <702611663@qq.com>
2022-12-10 20:26:57 +0800

    feat: 文件上传 request封装

---
+++ js/fileUpload/page/config/request.config.js
@@ -0,0 +1,5 @@
+export const BASE_URL = "http://127.0.0.1:8888";
+export const TIME_OUT = 10000;
+export const MULTIPART = "multipart/form-data";
+export const APP_JSON = "application/json";
+export const APP_CODED = "application/x-www-form-urlencoded";
---
+++ js/fileUpload/page/js/request.js
@@ -0,0 +1,71 @@
+import axios from "axios";
+import qs from "qs";
+import { BASE_URL, TIME_OUT, MULTIPART, APP_JSON, APP_CODED } from "../config/request.config";
+class myRequest {
+	instance;
+	interceptors;
+	constructor(config) {
+		this.instance = axios.create(config);
+		this.interceptors = config.interceptors;
+
+		this.instance.interceptors.request.use(this.interceptors.reqInterceptor);
+		this.instance.interceptors.response.use(this.interceptors.resInterceptor);
+
+		this.instance.interceptors.response.use(
+			res => {
+				return res.data;
+			},
+			err => {
+				return Promise.reject(err);
+			}
+		);
+	}
+	request(config) {
+		return new Promise((resolve, reject) => {
+			this.instance.request(config).then(
+				res => {
+					resolve(res);
+				},
+				err => {
+					reject(err);
+				}
+			);
+		});
+	}
+	get(url, config) {
+		return this.request({ ...config, method: "GET", url });
+	}
+	post(url, config) {
+		return this.request({ ...config, method: "POST", url });
+	}
+}
+
+export const LinRequestMultipart = new myRequest({
+	baseURL: BASE_URL,
+	timeout: TIME_OUT,
+	headers: {
+		"Content-Type": MULTIPART,
+	},
+});
+
+export const LinRequestAppCoded = new myRequest({
+	baseURL: BASE_URL,
+	timeout: TIME_OUT,
+	headers: {
+		"Content-Type": APP_CODED,
+	},
+	interceptors: {
+		reqInterceptor: config => {
+			config.data = qs.stringify(config.data);
+			return config;
+		},
+	},
+});
+
+export const LinRequestAppJson = new myRequest({
+	baseURL: BASE_URL,
+	timeout: TIME_OUT,
+	headers: {
+		"Content-Type": APP_JSON,
+	},
+});
